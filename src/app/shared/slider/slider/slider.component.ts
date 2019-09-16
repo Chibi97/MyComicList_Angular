9bdb9a9b6af45b311240fcf919d2a22f8d337cc0
Olja <olja.ivkovic97@gmail.com>
2019-09-16 16:49:42 +0200

    finish slider for home

--- src/app/shared/slider/slider/slider.component.ts
+++ src/app/shared/slider/slider/slider.component.ts
@@ -1,5 +1,5 @@
-import { Component, ContentChildren, QueryList, ViewChildren,
-   ElementRef, AfterViewInit, Input, ViewChild, HostListener } from '@angular/core';
+import { Component, ContentChildren, QueryList, ViewChildren, ElementRef, AfterViewInit,
+   Input, ViewChild, HostListener, OnInit } from '@angular/core';
 import { SliderItemDirective } from '../slider-item.directive';
 import { AnimationBuilder, animate, style, AnimationPlayer } from '@angular/animations';
 
@@ -12,19 +12,21 @@ export class SliderComponent implements AfterViewInit {
 
   @ContentChildren(SliderItemDirective) items: QueryList<SliderItemDirective>;
   @ViewChildren('li') itemElements: QueryList<ElementRef>;
-  @ViewChild('slider', {static: true}) slider: ElementRef;
+  @ViewChild('slider', { static: true }) slider: ElementRef;
   @Input() timing = '500ms ease-in';
   @Input() showControls = true;
 
-  constructor(private builder: AnimationBuilder) {
-    this.getScreenSize();
-   }
+  constructor(private builder: AnimationBuilder) { }
 
   itemWidth = 0;
-  calculated = {width: ''};
+  calculated = { 'max-width': '' };
   currentSlide = 0;
   player: AnimationPlayer;
-  screenWidth: any;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?) {
+    this.recalculateBounds();
+  }
 
   ngAfterViewInit() {
     this.itemElements.changes.subscribe(() => {
@@ -32,36 +34,26 @@ export class SliderComponent implements AfterViewInit {
     });
   }
 
-  @HostListener('window:resize', ['$event'])
-  getScreenSize(event?) {
-    this.screenWidth = window.innerWidth;
-    console.log(this.screenWidth);
-  }
-
   recalculateBounds() {
     setTimeout(() => {
-      // const itemWidth = this.itemElements.first.nativeElement.getBoundingClientRect().width;
-      this.calculated.width = `${this.screenWidth}px`;
-      this.itemWidth = this.screenWidth;
+      const itemWidth = this.itemElements.first.nativeElement.getBoundingClientRect().width;
+      this.calculated['max-width'] = `${itemWidth}px`;
+      this.itemWidth = itemWidth;
     });
   }
 
   buildAnimation(offset: number) {
     return this.builder.build([
-      animate(this.timing, style({transform: `translateX(-${offset}px)`}))
+      animate(this.timing, style({ transform: `translateX(-${offset}px)` }))
     ]);
   }
 
   next() {
-    console.log(this.items);
-
     this.currentSlide++;
     if (this.currentSlide > this.items.length - 1) {
       this.currentSlide = 0;
     }
-
     const offset = this.currentSlide * this.itemWidth;
-    console.log('offset is: ', offset);
     const myAnimation = this.buildAnimation(offset);
     this.player = myAnimation.create(this.slider.nativeElement);
     this.player.play();
@@ -74,7 +66,6 @@ export class SliderComponent implements AfterViewInit {
     }
 
     const offset = this.currentSlide * this.itemWidth;
-    console.log('offset is: ', offset);
     const myAnimation = this.buildAnimation(offset);
     this.player = myAnimation.create(this.slider.nativeElement);
     this.player.play();
