609f5e87fe41404447fcec74123860847c1b51b0
Olja <olja.ivkovic97@gmail.com>
2019-09-18 23:51:49 +0200

    comic validation done

--- src/app/admin/forms/comic-form/comic-form.component.ts
+++ src/app/admin/forms/comic-form/comic-form.component.ts
@@ -7,6 +7,8 @@ import { PublishersService } from 'src/app/services/publishers.service';
 import { AuthorsService } from 'src/app/services/authors.service';
 import { ComicsService } from 'src/app/services/comics.service';
 import { validateRegex } from 'src/app/shared/validators/regex.directive';
+import { isErrorResponse } from 'src/app/types/utils';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-comic-form',
@@ -14,6 +16,7 @@ import { validateRegex } from 'src/app/shared/validators/regex.directive';
   styleUrls: ['./comic-form.component.scss']
 })
 export class ComicFormComponent implements OnInit {
+  customError: string;
   constructor(
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<ComicFormComponent>,
@@ -63,6 +66,51 @@ export class ComicFormComponent implements OnInit {
     this.comicService.createComic(formData)
       .subscribe(() => {
         console.log('Comic created!');
+      },
+      (err: HttpErrorResponse) => {
+        if (isErrorResponse(err.error)) {
+          if (err.error.errors.Name) {
+            this.comicForm.controls.name.setErrors({ backend: err.error.errors.Name });
+          }
+
+          if (err.error.errors.Issues) {
+            this.comicForm.controls.issues.setErrors({ backend: err.error.errors.issues });
+          }
+
+          if (err.error.errors.Description) {
+            this.comicForm.controls.description.setErrors({ backend: err.error.errors.description });
+          }
+
+          if (err.error.errors.Genres) {
+            this.comicForm.controls.genres.setErrors({ backend: err.error.errors.genres });
+          }
+
+          if (err.error.errors.Authors) {
+            this.comicForm.controls.authors.setErrors({ backend: err.error.errors.authors });
+          }
+
+          if (err.error.errors.Publisher) {
+            this.comicForm.controls.publisher.setErrors({ backend: err.error.errors.publisher });
+          }
+
+          if (err.error.errors.Image) {
+            this.comicForm.controls.image.setErrors({ backend: err.error.errors.image });
+          }
+
+        } else {
+          this.customError = err.error.message;
+          if (this.customError.includes('Name')) {
+            this.comicForm.controls.name.setErrors({ invalid: true });
+          } else {
+            this.comicForm.setErrors({ invalid: true });
+          }
+
+          if (this.customError.includes('Image')) {
+            this.comicForm.controls.image.setErrors({ invalid: true });
+          } else {
+            this.comicForm.setErrors({ invalid: true });
+          }
+        }
       });
   }
 
@@ -97,5 +145,21 @@ export class ComicFormComponent implements OnInit {
     this.publisherService.getPublishers().subscribe((publishers) => {
       this.publishers = publishers;
     });
+  }
+
+  displayErrors(name: string) {
+    if (this.comicForm.get(name).errors) {
+      return this.comicForm.get(name).errors.backend;
+    }
+
+    return [];
+  }
+
+  clearError() {
+    if (this.customError !== '') {
+      this.customError = '';
+      this.comicForm.controls.name.setErrors(null);
+      this.comicForm.controls.origin.setErrors(null);
+    }
   }
 }
